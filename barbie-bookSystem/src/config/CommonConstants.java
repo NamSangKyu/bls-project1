c0e4ccfad53611e7bea62342f0869feec02ae1e4
ghgml1005@gmail.com <ghgml1005@gmail.com@14082aec-8ed9-44ce-ac62-8bab75b43630>
2012-12-03 07:03:43 +0000

    전체 파일 리스트

---
+++ barbie-bookSystem/src/config/CommonConstants.java
@@ -0,0 +1,29 @@
+package config;
+/**
+ * 페이징 처리시 사용할 공통 상수를 정의한 인터페이스 
+ * @author admin
+ *
+ */
+public interface CommonConstants {
+	/**
+	 * 한 페이지에서 보여질 게시물 수 
+	 */
+	int CONTENT_NUMBER_PER_PAGE=5;
+	/**
+	 * 한 페이지에 보여질 페이지 그룹의 페이지수 
+	 */
+	int PAGEGROUP_NUMBER_PER_PAGE=4;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
