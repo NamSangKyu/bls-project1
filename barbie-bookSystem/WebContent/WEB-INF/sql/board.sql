e46902fdecf9f369c53583cdad062a1bb9934307
ghgml1005@gmail.com <ghgml1005@gmail.com@14082aec-8ed9-44ce-ac62-8bab75b43630>
2012-12-04 06:24:22 +0000

    board.sql 수정

--- barbie-bookSystem/WebContent/WEB-INF/sql/board.sql
+++ barbie-bookSystem/WebContent/WEB-INF/sql/board.sql
@@ -1,15 +1,19 @@
 create table bls_board(
 	boardNo number primary key,
 	title varchar2(100) not null,
-	memberId varchar2(20) not null,
-	pass varchar2(40) ,
+	memberId varchar2(50) not null,
+	pass varchar2(50) ,
 	boardDate date not null,
-	count number not null,
+	count number default 0,
 	cont varchar2(4000) not null,
 	ref number not null,
 	restep number not null,
 	relevel number not null 
-	)
-	
-	create sequence bls_board_seq nocache;
-	
+)
+
+create sequence bls_board_seq nocache;
+
+insert into BLS_BOARD(boardNo, title, memberId, pass, boardDate, cont, ref, restep, relevel)
+values (bls_board_seq.nextval, '하하', 'bls1004', 'gg', sysdate, '으하하하', 1, 0, 0)
+
+select * from BLS_BOARD
