a7c9bd1768b13a80aaeb31a897f355b65e7d3690
ghgml1005@gmail.com <ghgml1005@gmail.com@14082aec-8ed9-44ce-ac62-8bab75b43630>
2012-12-04 05:36:47 +0000

    1차 수정

--- barbie-bookSystem/src/control/BoardController.java
+++ barbie-bookSystem/src/control/BoardController.java
@@ -1,5 +1,15 @@
 package control;
 
-public class BoardController {
+import model.BoardService;
+
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class BoardController extends MultiActionController {
+	private BoardService boardService;
+
+	public BoardController(BoardService boardService) {
+		super();
+		this.boardService = boardService;
+	}
 
 }
--- barbie-bookSystem/src/model/BoardDao.java
+++ barbie-bookSystem/src/model/BoardDao.java
@@ -1,5 +1,13 @@
 package model;
 
+import com.ibatis.sqlmap.client.SqlMapClient;
+
 public class BoardDao {
+	private SqlMapClient sqlMapClient;
+
+	public BoardDao(SqlMapClient sqlMapClient) {
+		super();
+		this.sqlMapClient = sqlMapClient;
+	}
 
 }
